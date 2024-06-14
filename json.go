5324f5835aa0813de9cf6263290d202405213bda
Mahdi Parivash <mahdiparivash@protonmail.com>
2024-06-14 10:31:11 +0330

    feat:handle json

--- json.go
+++ json.go
@@ -6,14 +6,26 @@ import (
 	"net/http"
 )
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Println("responding with 5xx error:", msg)
+	}
+	type errRespons struct {
+		Error string `json:"error"`
+	}
+	respondWithJson(w, code, errRespons{
+		Error: msg,
+	})
+}
+
 func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
 	dat, err := json.Marshal(payload)
 	if err != nil {
-		log.Println("failed to marshal JSON respones: %v", payload)
+		log.Printf("failed to marshal JSON respones: %v", payload)
 		w.WriteHeader(500)
 		return
 	}
 	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(200)
+	w.WriteHeader(code)
 	w.Write(dat)
 }
