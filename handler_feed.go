612a332c2e3f9c435622245a1012a8cc2184bb65
Mahdi Parivash <mahdiparivash@protonmail.com>
2024-06-15 22:49:55 +0330

    feat:Craete feed endpoint

---
+++ handler_feed.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/mhdph/rss-scraper/internal/database"
+)
+
+func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
+	type paramaters struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	}
+	json.NewDecoder(r.Body)
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := paramaters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing json: %v", err))
+		return
+	}
+
+	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+		Url:       params.URL,
+		UserID:    user.ID,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("coulden't create feed: %s", err))
+		return
+	}
+	respondWithJson(w, 201, databaseFeedtoFeed(feed))
+}
